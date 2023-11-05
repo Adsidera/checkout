18e482b3313c2c9701a595f7ffd551030b784920
Anna Costalonga <annamaria.costalonga@gmail.com>
2023-11-05 15:08:43 +0100

    Register products
    A convenient `Struct` object enables registering the three products, as a `PRODUCTS` constant. 
    Mapping product codes to products enables a fast lookup of the product, when we know only its product code.

--- lib/cash_calculator.rb
+++ lib/cash_calculator.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class CashCalculator
+  Product = Struct.new(:product_code, :name, :price, :discount)
+
+  PRODUCTS = {
+    'GR1' => Product.new('GR1','green tea', 311),
+    'SR1' => Product.new('SR1','strawberries', 500),
+    'CF1' => Product.new('CF1','coffee', 1123),
+  }
   def initialize(products)
     @products = products
   end
