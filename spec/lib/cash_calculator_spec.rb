a705ccc9e4515756f70a4b7417baf699db53de7d
Anna Costalonga <annamaria.costalonga@gmail.com>
2023-11-05 16:12:11 +0100

    Second scenario - Strawberries
    In a scenario with more than 3 strawberries,
    this logic is not enough. Test fails with wrong calculation.

--- spec/lib/cash_calculator_spec.rb
+++ spec/lib/cash_calculator_spec.rb
@@ -3,21 +3,27 @@
 require 'cash_calculator'
 
 RSpec.describe CashCalculator do
-  subject(:calculator) { described_class.new(products) }
+  subject(:calculator) { described_class.new(order) }
 
   describe '#total' do
 
     context 'with more than 1 green tea' do
-      let(:products) { %w[GR1 GR1] }
+      let(:order) { %w[GR1 GR1] }
 
       it 'calculates total price applying a buy-one-get-one-free discount' do
         expect(calculator.total).to eq 3.11
       end
     end
 
-    # context 'with more than 3 strawberries' do
+    context 'with more than 3 strawberries' do
+      let(:order) do
+        %w[SR1 SR1 GR1 SR1]
+      end
 
-    # end
+      it 'applies 10% bulk discount to strawberries' do
+        expect(calculator.total).to eq 16.61
+      end
+    end
 
     # context 'with more than 3 coffees' do
 
