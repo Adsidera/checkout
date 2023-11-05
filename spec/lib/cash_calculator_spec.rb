a6aa1a4b23c92c74d1faff477a356d050fc2dcab
Anna Costalonga <annamaria.costalonga@gmail.com>
2023-11-05 15:01:52 +0100

    Setup

---
+++ spec/lib/cash_calculator_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'cash_calculator'
+
+RSpec.describe CashCalculator do
+  subject(:calculator) { described_class.new(products) }
+
+  describe '#total' do
+
+    context 'with more than 1 green tea' do
+      let(:products) { %w[GR1 GR1] }
+
+      it 'calculates total price of order items' do
+        expect(calculator.total).to eq 3.11
+      end
+    end
+
+    # context 'with more than 3 strawberries' do
+
+    # end
+
+    # context 'with more than 3 coffees' do
+
+    # end
+end
