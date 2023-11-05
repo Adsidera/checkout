4ed902724ab77d5bc96fd3d14e4386d1f40fb3a6
Anna Costalonga <annamaria.costalonga@gmail.com>
2023-11-05 17:34:05 +0100

    UI - CLI checkout script

---
+++ checkout.rb
@@ -0,0 +1,11 @@
+require_relative 'lib/cash_calculator.rb'
+require 'colorize'
+
+list = ARGV
+
+if list.join("").match(",")
+  puts "Please use only GR1 SR1 or CF1 without commas in between, thanks! 😄🙏"
+else
+  calculator = CashCalculator.new(list)
+  puts "Your total price is ".colorize(:yellow) + "#{calculator.total}€".colorize(:cyan)
+end
