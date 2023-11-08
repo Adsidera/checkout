bfca1115ad881fd2338fcabe65abaec4812e6a97
Anna Costalonga <annamaria.costalonga@gmail.com>
2023-11-08 21:15:45 +0100

    UI - CLI checkout - printout
    It prints an invoice with subtotals.

--- checkout.rb
+++ checkout.rb
@@ -7,5 +7,7 @@ if list.join("").match(",")
   puts "Please use only GR1 SR1 or CF1 without commas in between, thanks! 😄🙏"
 else
   calculator = CashCalculator.new(list)
+  puts 'Your invoice'.colorize(:red)
+  calculator.printout
   puts "Your total price is ".colorize(:yellow) + "#{calculator.total}€".colorize(:cyan)
 end
