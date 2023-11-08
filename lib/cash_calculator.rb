14d9153efede49f64bf230e47f4d6773973d174c
Anna Costalonga <annamaria.costalonga@gmail.com>
2023-11-08 21:05:34 +0100

    Use percentages as fractions
    Discounts are  percentages expressed as fractions.
    This simplifies calculation and ensures its
    consistency.

--- lib/cash_calculator.rb
+++ lib/cash_calculator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'pry'
+# A class for calculating total checkout of a shopping cart
 class CashCalculator
   Product = Struct.new(:product_code, :name, :price)
 
@@ -8,36 +8,45 @@ class CashCalculator
     'GR1' => Product.new('GR1','green tea', 311),
     'SR1' => Product.new('SR1','strawberries', 500),
     'CF1' => Product.new('CF1','coffee', 1123),
-  }
+  }.freeze
 
   DISCOUNTS = {
-    'GR1' => { minimum_quantity: 2, discounted_quantity: 1},
-    'SR1' => { minimum_quantity: 3, discounted_price: 450},
-    'CF1' => { minimum_quantity: 3, discounted_price: 748.67}
-  }
+    'GR1' => { minimum_quantity: 2, discount: 0.5 },
+    'SR1' => { minimum_quantity: 3, discount: 0.1 },
+    'CF1' => { minimum_quantity: 3, discount: 0.3333 },
+  }.freeze
 
   def initialize(order)
     @order = order
     @quantities = order.group_by(&:itself).transform_values(&:count) # e.g.: {'SR1' => 2}
+    @subtotal = []
   end
 
   def total
-    subtotal = []
     @order.uniq.each do |item|
-      if @quantities[item] >= DISCOUNTS[item][:minimum_quantity]
-        subtotal << price_per(item) * quantity_per(item)
-      else
-        subtotal << PRODUCTS[item].price * @quantities[item]
-      end
+      @subtotal << subtotal_per(item)
     end
-    (subtotal.sum.to_f / 100).round(2)
+    (@subtotal.sum.to_f / 100).round(2)
+  end
+
+  def printout
+    puts '| PRODUCT   | € X QUANTITY | DISCOUNT | SUBTOTAL'
+    @order.uniq.each do |item|
+      puts "| #{PRODUCTS[item].name} | #{PRODUCTS[item].price.to_f / 100}€ X #{@quantities[item]} | #{(DISCOUNTS[item][:discount] * 100).to_i}% on > #{DISCOUNTS[item][:minimum_quantity]} | #{(subtotal_per(item).to_f/ 100).round(2)}€"
+    end
+  end
+
+  private
+
+  def subtotal_per(item)
+    if @quantities[item] >= DISCOUNTS[item][:minimum_quantity]
+      price_per(item) * @quantities[item]
+    else
+      PRODUCTS[item].price * @quantities[item]
+    end
   end
 
   def price_per(item)
-    DISCOUNTS[item][:discounted_price] || PRODUCTS[item].price
-  end
-
-  def quantity_per(item)
-    @quantities[item] - DISCOUNTS[item][:discounted_quantity].to_i
+    PRODUCTS[item].price - PRODUCTS[item].price * DISCOUNTS[item][:discount]
   end
 end
